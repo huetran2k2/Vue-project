281c82b390a63ac6c97b3b5a2d308888f7fe9d4f
Hue Tran <huett2501@gmail.com>
2022-08-09 22:21:11 +0700

    create post template

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShare, faComment, faThumbsUp, faHeart, faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import './assets/main.css'
+library.add(faShare);
+library.add(faComment);
+library.add(faThumbsUp);
+library.add(faHeart);
+library.add(faEllipsis);
 
-createApp(App).mount('#app')
+createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app");
