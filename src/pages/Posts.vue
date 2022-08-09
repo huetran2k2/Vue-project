c8bc305f0cb12ecd7891f11a5250a63ced37b7de
Hue Tran <huett2501@gmail.com>
2022-08-09 22:21:11 +0700

    create post template

---
+++ src/pages/Posts.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="content-post">
+      <div class="post-body">
+        <div class="user-wrap">
+          <div class="user-infor">
+            <div class="user-img">
+              <img src="../assets/images/profile-pic.png" class="user-pro-pic" alt="" />
+            </div>
+            <div class="user-content">
+              <h2 class="text-title">Tran Thi Hue</h2>
+              <br />
+              <span>Tue 9 at 10:43 AM.</span>
+            </div>
+          </div>
+          <div class="post-option-icons">
+            <font-awesome-icon icon="fa-solid fa-ellipsis" />
+          </div>
+        </div>
+
+        <div class="description">I'm building layout by HTML CSS</div>
+        <div class="post-detail">
+          <img src="../assets/images/feed-image-1.png" class="upload-img" alt="" />
+          <div class="like-comment-preview">
+            <div class="likes-preview">
+              <font-awesome-icon icon="fa-solid fa-thumbs-up" />
+              <font-awesome-icon icon="fa-solid fa-heart" />
+              <span class="count-likes">556k</span>
+            </div>
+            <div class="comment-share-preview">
+              <span>22.8k comments &ensp;</span>
+              <span>22.8k shares</span>
+            </div>
+          </div>
+        </div>
+        <div class="likes-comment-share-icons">
+          <div class="like" tabindex="1">
+            <font-awesome-icon icon="fa-solid fa-thumbs-up" />&nbsp;
+            <i>like</i>
+          </div>
+          <div class="comment">
+            <font-awesome-icon icon="fa-solid fa-comment" />&nbsp;
+            <i>comment</i>
+          </div>
+          <div class="share">
+            <font-awesome-icon icon="fa-solid fa-share" />&nbsp;
+            <i>share</i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Posts",
+};
+</script>
+<style lang="scss" scoped></style>
