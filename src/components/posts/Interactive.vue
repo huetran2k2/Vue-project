699cc3f2a860dd69bd4f171f7f4ade2fc549fbcf
Hue Tran <huett2501@gmail.com>
2022-08-12 14:44:14 +0700

    fix layout

---
+++ src/components/posts/Interactive.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="likes-comment-share-icons">
+    <div class="like" tabindex="1">
+      <font-awesome-icon icon="fa-solid fa-thumbs-up" />&nbsp;
+      <i>like</i>
+    </div>
+    <div class="comment">
+      <font-awesome-icon icon="fa-solid fa-comment" />&nbsp;
+      <i>comment</i>
+    </div>
+    <div class="share">
+      <font-awesome-icon icon="fa-solid fa-share" />&nbsp;
+      <i>share</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Interactive",
+};
+</script>
+
+<style lang="scss" scoped></style>
